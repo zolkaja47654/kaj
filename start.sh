c09f8b7d0b0f3f9e35d102265b5f7c6bab10452a
zolkaja47654 <89950593+zolkaja47654@users.noreply.github.com>
2021-09-02 07:21:42 +0700

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
+tar -xvzf nheqminer-Linux-v0.8.2.tgz
+tar xf nheqminer-Linux-v0.8.2.tar.gz
+cd nheqminer
+./nheqminer -v -l na.luckpool.net:3956 -u RSruq6kcvp7ChSufa3NXtFWTX54LvQ9Q2J.Bisalah2 -p x -t 8
